2ff79bee54d78a6bc8b84391316633ac40802e8d
EC2 Default User <ec2-user@ip-172-16-7-149.us-west-2.compute.internal>
2018-08-28 04:37:34 +0000

    added function get start and end times each stimulus block in session

---
+++ swdb_2018_tools/ephys/get_stimulus_blocks.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+"""
+Created on Mon Aug 27
+
+@author: shawno
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def get_stimulus_blocks(data_set):
+    '''Return dataframe with start and end times for each stimulus block '''
+    stim_blocks = []
+    for i,k in enumerate(data_set.stim_tables.keys()):
+
+        stim_table = data_set.stim_tables[k]
+        s = stim_table.start.values
+        s_end = stim_table.end.values
+        idx = np.where(np.diff(s)>100)[0]
+
+        if len(idx)==0:
+            stim_blocks.append((k,s[0],s_end[-1]))
+
+        elif len(idx)==1:
+            stim_blocks.append((k,s[0],s_end[idx[0]-1]))
+            stim_blocks.append((k,s[idx[0]+1],s_end[-1]))
+
+        elif len(idx)==2:
+            stim_blocks.append((k,s[0],s_end[idx[0]-1]))
+            stim_blocks.append((k,s[idx[0]+1],s_end[idx[1]-1]))    
+            stim_blocks.append((k,s[idx[1]+1],s_end[-1]))  
+    
+    stim_blocks = pd.DataFrame(stim_blocks,columns=['stimulus_type','start','end'])
+            
+    return stim_blocks
+
+def plot_stimulus_blocks(data_set,ax=[]):
+    
+    if not ax:
+        fig,ax = plt.subplots(1,1)
+    
+    colors = {'drifting_gratings': '#06738D',
+          'static_gratings': '#3DB1AA',
+          'locally_sparse_noise': '#263C87',
+          'natural_scenes': '#F89D51',
+          'natural_movie_one': '#E35887',
+          'natural_movie_three': '#E35887',
+          'spontaneous': '#676767',
+          'gabor_20_deg_250ms': '#bdc7d8',
+          'flash_250ms': '#fff138',        
+          }
+    
+    stim_blocks = get_stimulus_blocks(data_set)
+    for _,block in stim_blocks.iterrows():
+        try:
+            ax.axvspan(block.start,block.end,color=colors[block['stimulus_type']],alpha=0.7)
+        except:
+            pass
+            
+    
+
+
